1c42d04ed5c7c778384959e338ff6821187419a6
shehjad-dev <satauskz@gmail.com>
2022-11-07 17:21:55 +0600

    feat: all feat done for now

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const express = require("express");
+const router = express.Router();
+const {
+    registerUser,
+    getMe,
+    loginUser,
+} = require("../controllers/userController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/me", protect, getMe);
+
+module.exports = router;
